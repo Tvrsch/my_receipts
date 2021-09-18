a8d5aa8b64c495fee3426bc3b2aa48cb86de4320
Tvrsch <pozorss@mail.ru>
2021-09-18 22:15:52 +0300

    add shops merge confirmation

--- my_receipts/apps/shops/actions/merge_shops.py
+++ my_receipts/apps/shops/actions/merge_shops.py
@@ -1,31 +1,62 @@
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import ngettext
+from django.contrib.admin import helpers
+from django.contrib.admin.utils import model_ngettext
+from django.shortcuts import render
 
 
 def merge_shops(modeladmin, request, queryset):
+    print(request.POST)
     """Merge two shops with similar addresses"""
-    count = queryset.count()
-    if count != 2:
-        message = _("It's possible to merge only 2 shops, not %(count)s") % {
-            "count": count
-        }
-        modeladmin.message_user(request, message, level=messages.ERROR)
-    else:
-        master, slave = queryset.all()
-        receipts_updated = slave.receipts.update(shop=master)
-        slave.is_removed = True
-        slave.save()
+    if request.POST.get("merge_shops"):
+        count = queryset.count()
+        if count != 2:
+            message = _("It's possible to merge only 2 shops, not %(count)s") % {
+                "count": count
+            }
+            modeladmin.message_user(request, message, level=messages.ERROR)
+        else:
+            master, slave = queryset.all()
+            receipts_updated = slave.receipts.update(shop=master)
+            slave.is_removed = True
+            slave.save()
 
-        message = (
-            ngettext(
-                "%(count)d receipts moved to one shop.",
-                "%(count)d receipts moved to one shop.",
-                receipts_updated,
+            message = (
+                ngettext(
+                    "%(count)d receipts moved to one shop.",
+                    "%(count)d receipts moved to one shop.",
+                    receipts_updated,
+                )
+                % {"count": receipts_updated}
             )
-            % {"count": receipts_updated}
-        )
-        modeladmin.message_user(request, message)
+            modeladmin.message_user(request, message)
+    else:
+        title = "Merging"
+        objects_name = model_ngettext(queryset)
+        (
+            merging_objects,
+            model_count,
+            perms_needed,
+            protected,
+        ) = modeladmin.get_deleted_objects(queryset, request)
+        opts = modeladmin.model._meta
+        context = {
+            **modeladmin.admin_site.each_context(request),
+            "title": title,
+            "objects_name": str(objects_name),
+            "merging_objects": [merging_objects],
+            "model_count": dict(model_count).items(),
+            "queryset": queryset,
+            "opts": opts,
+            "action_checkbox_name": helpers.ACTION_CHECKBOX_NAME,
+            "media": modeladmin.media,
+        }
+
+        request.current_app = modeladmin.admin_site.name
+
+        # Display the confirmation page
+        return render(request, "shops/confirm_merge.html", context)
 
 
 merge_shops.short_description = _("Merge second shop to first")
